4acd04601285d40a573b159ba0893e4577fb5ea0
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-08-04 20:44:54 -0300

    Create backend

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import { send } from "process";
+import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.get("/", (request, response) => {
-  return response.send("Hello World");
-});
+app.use(routes);
 
 app.listen(3333);
