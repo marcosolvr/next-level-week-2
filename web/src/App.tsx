abc101004c7d2061a8e31fc3346fbdc038974b55
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-08-08 21:24:36 -0300

    Style initials pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Routes from "./routes";
+import "./assets/styles/global.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Routes />
     </div>
   );
 }
---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars1.githubusercontent.com/u/32722681?s=400&v=4"
+          alt="Nome do cara"
+        />
+        <div>
+          <strong>Nome do Cara</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratórios e por mudar a vida das
+        pessoas através das experiências.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="WhatsApp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
