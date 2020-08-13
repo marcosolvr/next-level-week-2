b5fc084ac969468f3cc7c4f2544393d7bb8d18f7
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-08-13 16:36:02 -0300

    Create and style TeacherForm for web

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+
 import PageHeader from "../../components/PageHeader";
+import Input from "../../components/Input";
+
+import warningIcon from "../../assets/images/icons/warning.svg";
+
+import "./styles.css";
 
 function TeacherForm() {
   return (
@@ -8,6 +14,32 @@ function TeacherForm() {
         title="Que incrível que você quer dar aulas."
         description="O primeiro passo é preencher esse formulário de inscrição"
       />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso important" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
